560a18077bc17656a7fe694d42e48c4cee03a381
Nguyen Huu Huy <daxua1234567@gmail.com>
2023-09-17 23:41:51 +0700

    Bring up RisingOSS

--- lineage_blueline.mk
+++ lineage_blueline.mk
@@ -4,24 +4,26 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit some common Spark stuff.
-$(call inherit-product, vendor/spark/config/common_full_phone.mk)
+# Inherit some common Rising OS stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 TARGET_DISABLE_EPPE := true
 TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_GAPPS_ARCH := arm64
-SPARK_BUILD_TYPE := UNOFFICIAL
-TARGET_EXCLUDES_AUDIOFX := true
+RISING_MAINTAINER := "26.pluvieux"
+RISING_PACKAGE_TYPE := "GAPPS"
+TARGET_BUILD_APERTURE_CAMERA := false
+TARGET_ENABLE_BLUR := true
 TARGET_HAS_UDFPS := false
-TARGET_USES_BLUR := false
+TARGET_USE_PIXEL_FINGERPRINT := false
+TARGET_EXCLUDES_AUDIOFX := true
 
 #Gapps option 
 TARGET_SUPPORTS_GOOGLE_RECORDER := false
 TARGET_INCLUDE_STOCK_ARCORE := false
-TARGET_INCLUDE_LIVE_WALLPAPERS := true
-TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_SUPPORTS_CALL_RECORDING := false
 TARGET_FACE_UNLOCK_SUPPORTED := true
-WITH_GAPPS=true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_SUPPORTS_QUICK_TAP := true
+WITH_GMS=true
 
 
 # Inherit device configuration
@@ -30,11 +32,9 @@ $(call inherit-product, device/google/crosshatch/aosp_blueline.mk)
 include device/google/crosshatch/blueline/device-lineage.mk
 
 # Device identifier. This must come after all inclusions
-PRODUCT_MANUFACTURER := Google
-PRODUCT_NAME := spark_blueline
-PRODUCT_DEVICE := blueline
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel 3
+PRODUCT_NAME := lineage_blueline
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2160
