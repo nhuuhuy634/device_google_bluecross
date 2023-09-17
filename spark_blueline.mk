e655b001489bcd5083c635b22f6f3140c14f72a2
nhuuhuy634 <daxua1234567@gmail.com>
2023-09-17 23:15:34 +0700

    Based on LineageOS

---
+++ spark_blueline.mk
@@ -0,0 +1,49 @@
+#
+# Copyright (C) 2018-2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit some common Spark stuff.
+$(call inherit-product, vendor/spark/config/common_full_phone.mk)
+TARGET_DISABLE_EPPE := true
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_GAPPS_ARCH := arm64
+SPARK_BUILD_TYPE := UNOFFICIAL
+TARGET_EXCLUDES_AUDIOFX := true
+TARGET_HAS_UDFPS := false
+TARGET_USES_BLUR := false
+
+#Gapps option 
+TARGET_SUPPORTS_GOOGLE_RECORDER := false
+TARGET_INCLUDE_STOCK_ARCORE := false
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_SUPPORTS_CALL_RECORDING := false
+TARGET_FACE_UNLOCK_SUPPORTED := true
+WITH_GAPPS=true
+
+
+# Inherit device configuration
+$(call inherit-product, device/google/crosshatch/aosp_blueline.mk)
+
+include device/google/crosshatch/blueline/device-lineage.mk
+
+# Device identifier. This must come after all inclusions
+PRODUCT_MANUFACTURER := Google
+PRODUCT_NAME := spark_blueline
+PRODUCT_DEVICE := blueline
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Pixel 3
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2160
+TARGET_SCREEN_WIDTH := 1080
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_PRODUCT=blueline \
+    PRIVATE_BUILD_DESC="blueline-user 12 SP1A.210812.016.C2 8618562 release-keys"
+
+BUILD_FINGERPRINT := google/blueline/blueline:12/SP1A.210812.016.C2/8618562:user/release-keys
+
+$(call inherit-product, vendor/google/blueline/blueline-vendor.mk)
