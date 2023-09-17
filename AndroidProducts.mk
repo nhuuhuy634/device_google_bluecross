57f7ee201c1694200b782529ef13598fd5e142f1
Nguyen Huu Huy <daxua1234567@gmail.com>
2023-09-17 23:35:46 +0700

    Bring up again RisingOSS

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -20,12 +20,12 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aosp_crosshatch_hwasan.mk \
     $(LOCAL_DIR)/aosp_blueline_hwasan.mk \
     $(LOCAL_DIR)/vf/aosp_crosshatch_vf.mk \
-    $(LOCAL_DIR)/spark_blueline.mk \
-    $(LOCAL_DIR)/spark_crosshatch.mk
+    $(LOCAL_DIR)/lineage_blueline.mk \
+    $(LOCAL_DIR)/lineage_crosshatch.mk
 
 COMMON_LUNCH_CHOICES := \
     aosp_crosshatch-userdebug \
     aosp_blueline-userdebug \
     aosp_crosshatch_vf-userdebug \
-    spark_blueline-userdebug \
-    spark_crosshatch-userdebug
+    lineage_blueline-userdebug \
+    lineage_crosshatch-userdebug
