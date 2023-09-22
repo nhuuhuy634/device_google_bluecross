3787d290e424f25dbed143b012e8a3f42a5c7849
Nguyen Huu Huy <daxua1234567@gmail.com>
2023-09-22 11:43:24 +0700

    Optimize Launcher3 and Settings for speed
    from commit : https://github.com/ProjectElixir-Devices/device_google_crosshatch/commit/6a3847e6f98805e26ac2198f4e1b8dc384cda711

--- device.mk
+++ device.mk
@@ -808,6 +808,10 @@ endif
 # Preopt SystemUI
 PRODUCT_DEXPREOPT_SPEED_APPS += SystemUIGoogle  # For internal
 PRODUCT_DEXPREOPT_SPEED_APPS += SystemUI  # For AOSP
+PRODUCT_DEXPREOPT_SPEED_APPS += \
+    Launcher3QuickStep \
+    NexusLauncherRelease \
+    Settings
 
 # Compile SystemUI on device with `speed`.
 PRODUCT_PROPERTY_OVERRIDES += \
